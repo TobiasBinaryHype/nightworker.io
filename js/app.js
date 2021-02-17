7425b9100109ff273e1e0ed2bf914c4a5b0a431c
Tobias Kokesch <pandamacia@gmx.de>
2021-02-17 21:19:24 +0100

    Random Bugfix

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 // Smooth Scroll Jump Marker
 document.querySelectorAll('a').forEach(element => {
     element.addEventListener('click', event => {
-        event.preventDefault();
-
         const hash = event.target.hash;
         
-        document.querySelector(hash).scrollIntoView({
-            behavior: 'smooth'
-        })
+        if (hash !== '') {
+            event.preventDefault();
+            document.querySelector(hash).scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
     })
 })
 
